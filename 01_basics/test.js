0f9bafe18ba51b9157ae33e53b7520622540909e
Raj Deshmukh <rajdeshmukh282@gmail.com>
2025-06-01 17:14:41 +0530

    01_basics

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,4 +1,5 @@
-console.log("Hello, World!");// This is a test file
-console.log("This is a test file for the code completion feature.");
-//this is how i waste my time on github 
-//sdare
+const account_id = 23;// This is a constant and cannot be changed
+let account_name = "John Doe";// This is a variable that can be changed
+var account_balance = 1000.50;// This is a variable that can be changed
+account_city = "New York";// This is a variable that can be changed, but it is not declared with let or var
+console.log(account_id);
