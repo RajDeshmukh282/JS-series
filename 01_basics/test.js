0abce6ac8bb1edfcdd721d322bc0a9f57f5dd3d9
Raj Deshmukh <rajdeshmukh282@gmail.com>
2025-06-01 17:19:16 +0530

    Print

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -2,4 +2,4 @@ const account_id = 23;// This is a constant and cannot be changed
 let account_name = "John Doe";// This is a variable that can be changed
 var account_balance = 1000.50;// This is a variable that can be changed
 account_city = "New York";// This is a variable that can be changed, but it is not declared with let or var
-console.log(account_id);
+console.log(account_id,account_balance,account_name,account_city);// This will print the values of the variables to the console
