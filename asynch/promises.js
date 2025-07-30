1d2914bf3e8f4240567a5a1bfade3a75d14591f7
Raj Deshmukh <rajdeshmukh282@gmail.com>
2025-07-31 01:07:02 +0530

    Refactor promise handling and improve error management in promises.js

--- asynch/promises.js
+++ asynch/promises.js
@@ -1,36 +1,61 @@
+// // const promiseOne = new Promise((resolve, reject) => {
+// //     // The asynchronous operation starts here
+// //     setTimeout(() => {
+// //         console.log('Async task is complete');
+
+// //         // NOW we resolve the promise, because the task is done.
+// //         resolve();
+// //     }, 1000);
+// // });
+
+// // // Now we consume the promise
+// // promiseOne.then(() => {
+// //     // This .then() will only run AFTER the promise has been resolved.
+// //     // which is after 1 second.
+// //     console.log("Promise consumed!");
+// // });
 // const promiseOne = new Promise((resolve, reject) => {
-//     // The asynchronous operation starts here
-//     setTimeout(() => {
-//         console.log('Async task is complete');
-
-//         // NOW we resolve the promise, because the task is done.
-//         resolve();
-//     }, 1000);
+//   setTimeout(() => {
+//     const error = false;
+//     if (!error) {
+//       console.log("Promise Resolved ");
+//       resolve("User data fetched");
+//     } else {
+//       reject("Error Occurred. Try again");
+//     }
+//   }, 1000);
 // });
 
-// // Now we consume the promise
-// promiseOne.then(() => {
-//     // This .then() will only run AFTER the promise has been resolved.
-//     // which is after 1 second.
-//     console.log("Promise consumed!");
-// });
-const promiseOne = new Promise((resolve, reject) => {
-  const error = false;
-  if (!error) {
-    setTimeout(() => {
-      console.log("Promise Resolved ");
-      resolve("User data fetched");
-    }, 1000);
-  } else {
-    reject("Error Occured Try again");
-  }
+// promiseOne
+//   .then((Data) => {
+//     console.log("Promise Consumed");
+//     console.log(Data);
+//   })
+//   .catch((error) => {
+//     console.log("Error!!");
+//     console.log(error);
+//   });
+
+const promiseFour = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const error = true;
+    if (!error) {
+      console.log("Fetching Data");
+      resolve({ name: "Raj", age: 23 });
+    } else {
+      reject("Error in Promise");
+    }
+  }, 1000);
 });
-promiseOne
+promiseFour
   .then((Data) => {
     console.log("Promise Consumed");
-    console.log(Data);
+    console.log(Data.name);
   })
   .catch((error) => {
     console.log("Error!!");
     console.log(error);
-  });
+  })
+  .finally(()=>{
+    console.log('Task complete')
+  })
